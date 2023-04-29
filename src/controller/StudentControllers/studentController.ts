c1dce99c8f580e14dbaa49e76dff8e0804180d53
GustavoBalsamo <gubalsamo@hotmail.com>
2023-04-29 14:38:06 -0300

    student Controller feita

---
+++ src/controller/StudentControllers/studentController.ts
@@ -0,0 +1,27 @@
+import { Request, Response } from 'express';
+import { GetStudent } from './getStudent';
+import { SaveStudent } from './saveStudent';
+import { UpdateStudent } from './updateStudent';
+import { RemoveStudent } from './removeStudent';
+
+export class StudentController {
+  public async getStudent(request: Request, response: Response): Promise<Response> {
+    const getStudent = new GetStudent();
+    return getStudent.handle(request, response);
+  }
+
+  public async saveStudent(request: Request, response: Response): Promise<Response> {
+    const saveStudent = new SaveStudent();
+    return saveStudent.handle(request, response);
+  }
+
+  public async updateStudent(request: Request, response: Response): Promise<Response> {
+    const updateStudent = new UpdateStudent();
+    return updateStudent.handle(request, response);
+  }
+
+  public async removeStudent(request: Request, response: Response): Promise<Response> {
+    const removeStudent = new RemoveStudent();
+    return removeStudent.handle(request, response);
+  }
+}
